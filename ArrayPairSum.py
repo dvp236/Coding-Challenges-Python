220bfbcea03f2b6dd96f940a1578a76af6990101
Dharmik Patel <dvp236@nyu.edu>
2017-01-30 21:17:40 -0500

    *updated TwoSum

--- ArrayPairSum.py
+++ ArrayPairSum.py
@@ -1,46 +1,26 @@
+print "Bless me Lord Swaminarayan!"
 
-# coding: utf-8
-
-# In[1]:
-
-print "Swaminarayan!"
-
-
-# In[37]:
 
 def getPairs(array,number):
-    
-    if len(array) <= 2:
+
+    if len(array) < 2:
         return
-    
+
     seen = set()
     output = set()
-    
+
     for item in array:
         target = number - item
         #if can not make a pair, add it to set for later use
         if target not in seen:
             seen.add(item)
-        
+
         else:
             output.add((min(target,item),max(target,item)))
-                       
+
     return output
 
 
 
-# In[41]:
-
 result = getPairs([1,2,3,6,5,4],7)
 print "\n".join(map(str,list(result)))
-
-
-# In[ ]:
-
-
-
-
-# In[ ]:
-
-
-
