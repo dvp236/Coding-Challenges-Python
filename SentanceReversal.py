54665981d6b9f0eab30d90f9f469116e9c68711f
Dharmik Patel <dvp236@nyu.edu>
2017-01-31 22:06:51 -0500

    clean up- sentance reversal

--- SentanceReversal.py
+++ SentanceReversal.py
@@ -1,12 +1,6 @@
 
-# coding: utf-8
-
-# In[1]:
-
 print "Jsn _/\_"
 
-
-# In[2]:
 
 def rev1(s):
     return " ".join(reversed(s.split()))
@@ -15,26 +9,20 @@ def rev2(s):
     return " ".join(s.split()[::-1])
 
 
-# In[8]:
 
 print rev1('patel dharmik am i  ')
-
-
-# In[6]:
 
 print rev2("i am dharmik     patel")
 
 
-# In[9]:
-
-#manually 
+#manually
 #since we have to ignore excess spaces
 def rev3(s):
-    
+
     words = []
     spaces = [' ']
     i = 0
-    
+
     while(i < len(s)):
         if s[i] not in spaces:
             word_start = i
@@ -42,18 +30,9 @@ def rev3(s):
                 i+=1
             words.append(s[word_start:i])
         i+=1
-        
+
     return " ".join(reversed(words))
-            
-    
-
-
-# In[10]:
-
-print rev2("i am dharmik     patel")
-
-
-# In[ ]:
 
 
 
+print rev3("i am dharmik     patel")
